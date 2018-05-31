a95642df23be42a88dc51b77e8db3a6794fd7a59
kentaro.mihara <miha.ken.19@gmail.com>
2018-05-31 19:31:35 +0000

    toppage sidebar

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -5,6 +5,7 @@ class ToppagesController < ApplicationController
       @post = current_user.posts.build
       @posts = Post.all.order("created_at DESC").page(params[:page])
       
+      
     end
   end
 end
